2f321d02a88cd48de858683289f9a581ba1f21a6
Quentin <quentin.caflo@gmail.com>
2025-03-06 14:29:26 +0100

    feat(footer): add footer without responsive styling

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -8,10 +8,12 @@ import { initMenuToggler } from "./components/menuToggler";
 import { loginUser } from "./components/login";
 import { logoutUser } from "./components/logout";
 import { setAuthButtonState } from "./components/setAuthButtonState";
+import { initializeFooterDate } from "./components/dateUtils";
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Project loaded successfully!");
 
+  initializeFooterDate();
   initMenuToggler();
   setAuthButtonState();
   loginUser();

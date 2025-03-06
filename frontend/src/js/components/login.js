5e7e857865e20bb7ab2f613b588581a92f1bc65f
Quentin <quentin.caflo@gmail.com>
2025-03-06 10:45:16 +0100

    feat(auth): add the required field message

--- frontend/src/js/components/login.js
+++ frontend/src/js/components/login.js
@@ -8,9 +8,31 @@ export function loginUser() {
   loginForm.addEventListener("submit", async (event) => {
     event.preventDefault();
 
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    const inputs = {
+      email: document.getElementById("email"),
+      password: document.getElementById("password"),
+    };
     const rememberMe = document.getElementById("remember-me").checked;
+
+    let hasError = false;
+    Object.values(inputs).forEach((input) => {
+      removeRequiredFieldMessage(input);
+
+      if (!input.value.trim()) {
+        showRequiredFieldMessage(input);
+        hasError = true;
+      }
+    });
+
+    if (!email) {
+      showRequiredFieldMessage(inputs.email);
+      hasError = true;
+    }
+    if (!password) {
+      showRequiredFieldMessage(inputs.password);
+      hasError = true;
+    }
+    if (hasError) return;
 
     try {
       const response = await fetch("http://localhost:5000/api/auth/login", {
@@ -31,4 +53,26 @@ export function loginUser() {
       console.error("Erreur:", error);
     }
   });
+}
+
+function showRequiredFieldMessage(input) {
+  const fieldContainer = input.closest(".auth-form__field");
+
+  if (fieldContainer.querySelector(".auth-form__field--required")) return;
+
+  const errorDiv = document.createElement("div");
+  errorDiv.classList.add("auth-form__field--required");
+
+  const message = document.createElement("span");
+  message.textContent = "This field is required";
+
+  errorDiv.appendChild(message);
+  fieldContainer.appendChild(errorDiv);
+}
+
+function removeRequiredFieldMessage(input) {
+  const fieldContainer = input.closest(".auth-form__field");
+  const errorDiv = fieldContainer?.querySelector(".auth-form__field--required");
+
+  if (errorDiv) errorDiv.remove();
 }
